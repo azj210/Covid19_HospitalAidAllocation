4e47c14fd204e58c2e9223657d2f5bf45d15f93a
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-09 14:32:54 -0400

    Add Hospital Beds Mapper and Reducer

---
+++ Covid19Project/HospBeds/HospBedMapper.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class HospBedMapper
+    extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+    
+	    String[] line = value.toString().toLowerCase().split(",");
+	    for (int i = 0; i<line.length; i++){
+	    	//when you encounter an int aka hospital beds then write
+	    	try{
+	    		int result = Integer.parseInt(line[i]);
+	    		//key is city!state and value is hospital beds of this particular line aka hospital
+	    		String keyVal = line[i-1] + "!" + line[line.length -1]; 
+	    		context.write(new Text(keyVal), new IntWritable(result)); 
+	    		break;	
+	    	}
+	    	catch(Exception e){
+
+	    	}
+	    }
+	}
+}
+
+
+
+
---
+++ Covid19Project/HospBeds/HospBedReducer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class HospBedReducer
+    extends Reducer<Text, IntWritable, Text, IntWritable> {
+  
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+    
+    int totalValue = 0;
+    for (IntWritable value : values) {
+      totalValue += value.get();
+    }
+    //key is city!state and value is hospital beds for all hospitals in that city!state
+    context.write(key, new IntWritable(totalValue));
+  }
+}
+
+
+
+
