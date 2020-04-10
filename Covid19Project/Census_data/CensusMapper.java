ed08e5d160203b05dd1435dcdc163b62f276724c
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-10 15:27:20 -0400

    Added census reducer

--- Covid19Project/Census_data/CensusMapper.java
+++ Covid19Project/Census_data/CensusMapper.java
@@ -20,12 +20,11 @@ public class CensusMapper
     for (int i=0; i < AGEGRP.length; i++){
       if (line[5].equals("11") && line[6].equals(AGEGRP[i])){
         //indexes: STNAME -3, CTYNAME -4, YEAR -5, AGEGRP -6, TOT_POP -7, TOT_MALE -8
-
-        
         //setting the key value pair
         String ST_CTY = line[3] + "!" + line[4];
         String TotPop_and_Male = line[7] + "!" + line[8];
         
+        //key is STNAME!CTYNAME and value is TOT_POP!TOT_MALE of a particular age group in the 60+ age group range
         context.write(new Text(ST_CTY), new Text(TotPop_and_Male));
         break;
       } 
