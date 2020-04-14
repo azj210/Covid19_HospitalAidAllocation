2f95620ad6ebf6c370dc06eed566879bbdc5da2a
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-14 03:13:24 -0400

    Resolve New York City County edge case

--- Covid19Project/Covid19Data/CovidMapper.java
+++ Covid19Project/Covid19Data/CovidMapper.java
@@ -14,10 +14,19 @@ public class CovidMapper
 	    String[] line = value.toString().toLowerCase().split(",");
 	    if (line.length >1){
 	    	if ((!line[1].equals("admin2")) && (!line[1].equals("unassigned")) && (!line[2].equals("diamond princess")) && (line[3].equals("us")) && (!line[1].equals("")) && (!line[2].equals(""))){
-		   		String keyVal = line[1] + "!" + line[2];
-		   		String result = line[7] + "!" + line[8];
-		   		//key is county!state and value is confirmed_cases!deaths		
-		    	context.write(new Text(keyVal), new Text(result)); 
+		   		//new york city instead of new york county edge case
+		   		if (line[1].equals("new york city")){
+		   			String keyVal = "new york" + "!" + line[2];
+			   		String result = line[7] + "!" + line[8];
+			   		//key is county!state and value is confirmed_cases!deaths		
+			    	context.write(new Text(keyVal), new Text(result)); 
+		   		}
+		   		else{
+		   			String keyVal = line[1] + "!" + line[2];
+			   		String result = line[7] + "!" + line[8];
+			   		//key is county!state and value is confirmed_cases!deaths		
+			    	context.write(new Text(keyVal), new Text(result)); 
+		   		}
 		   	}
 	    }
 	}
