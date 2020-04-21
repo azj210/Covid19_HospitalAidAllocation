ecfc8b90fadf5391495ecd52397872d211cebdf4
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-21 13:43:03 -0400

    Changed Deaths Prediction
    Changed the way deaths are predicted from linear(lagCases,lagDeaths) to polynomial(time,deaths)

--- Covid19Project/Covid19Data/CovidScraper.py
+++ Covid19Project/Covid19Data/CovidScraper.py
@@ -15,43 +15,39 @@ def editDict(dictInput, link):
 			#line[7] is confirmed and line[8] is deaths
 			dictInput[line[1].lower() + ', ' + line[2].lower()].append([line[7],line[8]])
 
-def predict(x, y, dim, pred):
-    if dim == 2:
-        #quadratic regression: predicting cases 3 days out
-        coeffs = np.polyfit(x, y, dim)
-        futureCases = []
-        for i in range(len(x),len(x)+4):
-            case = round((coeffs[0] * (i**2)) + (coeffs[1] * i) + coeffs[1])
-            if case <= 0:
-                futureCases.append(0)
-            else:
-                futureCases.append(case)
-        return futureCases
-    else:
-        #linear regression: predicting deaths for predicted cases
-        coeffs2 = np.polyfit(x, y, dim)
-        deaths = round((coeffs2[0] * pred) + coeffs2[1])
-        if deaths <= 0:
-            return 0
-        else:   
-            return (deaths)
+def predict(x, y, pred):
+    #quadratic regression: predicting cases 3 days out
+    coeffs = np.polyfit(x, y, 2)
+    futureCases = []
+    for i in range(len(x),len(x)+pred):
+        case = int(round((coeffs[0] * (i**2)) + (coeffs[1] * i) + coeffs[1]))
+        if case <= 0:
+            futureCases.append(0)
+        else:
+            futureCases.append(case)
+    return futureCases
+    
 
 def main():
     start = date(2020,3,22)
-    end = date(2020,4,19)
-    #end = date.today()
+    end = date(2020,4,20)
     delta = timedelta(days=1)
     tracker = defaultdict(list)
 	#loop through covid files from 3/22/2020 to today
-	#note: use the previous day if today's file has not been uploaded yet
+    allDates = []
     while start <= end:
         thisDate = start.strftime('%m-%d-%Y')
         link = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + thisDate + '.csv'
         editDict(tracker, link)
+        allDates.append(thisDate)
         start += delta
-        
+    predDates = []
+    for i in range(4):
+        end += delta
+        predDates.append(end.strftime('%m-%d-%Y'))
+        allDates.append(end.strftime('%m-%d-%Y'))
     for i in tracker.keys():
-        if len(tracker[i]) > 7:
+        if len(tracker[i]) > 2:
             data = tracker[i]
             time = [t for t in range (1,len(tracker[i]))]
             #new cases and new deaths
@@ -59,30 +55,14 @@ def main():
             deaths = []
             for j in range(1,len(data)):
                 cases.append(int(data[j][0]) - int(data[j-1][0]))
-                deaths.append(int(data[j][1]) - int(data[j-1][1]))
-            #take into account lag between cases and deaths
-            lagCases = cases[:len(cases)-3]
-            lagDeaths = deaths[3:]
-            #a = Polynomial.fit(time, cases, 2)
-            #plt.plot(*a.linspace())
-            
+                deaths.append(int(data[j][1]) - int(data[j-1][1]))         
             #first calculate number of new cases 4 days out
-            newCases = predict(time, cases, 2, 0)
-            newDeaths = []
-            #error handling. Less than 12 predicted cases over 4 days causes an error in linear least squares convergence. 
-            if sum(newCases) <= 11:
-                newDeaths = [0,0,0,0]
-            else:
-                #next calculate number of new deaths each of the 4 days
-                for z in newCases:
-                    newDeaths.append(predict(lagCases, lagDeaths, 1, z))
+            newCases = predict(time, cases, 4)
+            newDeaths = predict(time, deaths, 4)
             print(i, newCases, newDeaths)
         #error handling for if there is 1 week or less worth of data
         else:
             print(i, [0]*4, [0]*4)
-
-
-
 
 if __name__ == '__main__':
 	main()
