2e429883dfe7be78dbd1f41e648c3e637b443c0d
azj210 <ajiang1098@gmail.com>
2020-04-25 23:18:09 -0400

    Cleaned code & daily update

--- Covid19Project/HospBedsPrediction/CovidScraper.py
+++ Covid19Project/HospBedsPrediction/CovidScraper.py
@@ -21,7 +21,7 @@ def predict(x, y, pred):
     #quadratic regression: predicting cases for pred days out
     coeffs = np.polyfit(x, y, 2)
     futureCases = []
-    for i in range(len(x),len(x)+pred):
+    for i in range(len(x), len(x)+pred):
         case = int(round((coeffs[0] * (i**2)) + (coeffs[1] * i) + coeffs[1]))
         if case <= 0:
             futureCases.append(0)
@@ -29,42 +29,39 @@ def predict(x, y, pred):
             futureCases.append(case)
     return futureCases
 
-def generatePredict(tracker, condition, writer, days):
+def generatePredict(tracker, condition, days):
     res = defaultdict(int)
     for i in tracker.keys():
-        place = i.split(",")
         #error handling for if there is 2 days or less worth of data
         if len(tracker[i]) > 2:
             data = tracker[i]
-            time = [t for t in range (1,len(tracker[i]))]
+            time = [t for t in range (1, len(tracker[i]))]
             cases = []
             if condition == "cases":
-                for j in range(1,len(data)):
+                for j in range(1, len(data)):
                     cases.append(max(0,(int(data[j][0]) - int(data[j-1][0]))))
             else:
-                for j in range(1,len(data)):
+                for j in range(1, len(data)):
                     cases.append(max(0,(int(data[j][1]) - int(data[j-1][1]))))  
             #calculate number of new cases or deaths "days" days out
             futureCases = predict(time, cases, days)
-            writer.writerow([place[0],place[1]] + futureCases)
             cases += futureCases
             res[i] = cases
     return res
 
 #c - time series of new cases from 3/22 to "days" days from the end date. d is corresponding new death time series
-def bedsNeeded(c, d, tracker, days, writer):
+def bedsNeeded(c, d, tracker, days):
     beds = defaultdict(list)
     dischargeAmt = {15:(1/4), 21:(1/3), 26:(1/2), 32:1}
     #run simulation for each county
     for i in c.keys():
-        place = i.split(",")
         #Of the initial reported numbers, around 75% are in the hospital
         beds[i].append(int(round(int(tracker[i][0][0]) * .75)))
-        #30% of people need hospitalization and of those 30%, it can be assumed that 75% will be discharged
-        toDischarge = [round(z * 0.3 * 0.75) for z in copy.deepcopy(c[i])] 
+        #1/3 of people need hospitalization and of those 1/3%, it can be assumed that 75% will be discharged
+        toDischarge = [round(z * (1/3) * 0.75) for z in copy.deepcopy(c[i])] 
         #loop through all days from day 1 to today
         for j in range(len(c[i])):
-            yestH, newHosp, deaths = beds[i][j], 0.3 * c[i][j], 0.8 * d[i][j] 
+            yestH, newHosp, deaths = beds[i][j], (1/3) * c[i][j], 0.8 * d[i][j] 
             #calculate people to discharge today
             todayDischarge = 0
             for k in dischargeAmt.keys():
@@ -79,15 +76,12 @@ def bedsNeeded(c, d, tracker, days, writer):
         if len(beds[i]) < days:
             extra = [0] * (days - len(beds[i]))
             beds[i] = extra + beds[i]
-        writer.writerow([place[0],place[1]] + beds[i])     
     return beds
 
 def main():
-    start = date(2020,3,22)
-    end = date(2020,4,24)
-    delta = timedelta(days=1)
+    start, end, delta = date(2020,3,22), date(2020,4,25), timedelta(days=1)
     tracker = defaultdict(list)
-    days, days2 = 90, 90
+    days, days2 = 3, 3
     dates = []
 	#loop through all available covid files from 3/22/2020 to today
     while start <= end:
@@ -100,30 +94,20 @@ def main():
     for i in range(days2):
         dates.append(start.strftime('%m-%d-%Y'))
         start += delta
-    with open('PredictCases.csv', mode='w+') as f:
-        writer = csv.writer(f, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        c = generatePredict(tracker, "cases", writer, days2)   
-    with open('PredictDeaths.csv', mode='w+') as f:
-        writer = csv.writer(f, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        d = generatePredict(tracker, "deaths", writer, days2)
-    with open('PredictBeds.csv', mode='w+') as f:
-        writer = csv.writer(f, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        b = bedsNeeded(c, d, tracker, days, writer)
-    #converting bedsNeeded to json
+    c, d = generatePredict(tracker, "cases", days2), generatePredict(tracker, "deaths", days2)
+    b = bedsNeeded(c, d, tracker, days)
+    #converting bedsNeeded to json to read into spark dataframe
     forwJson = []
     for i in b.keys():
-        count = 0
         place = i.split(",")
         toAppend = OrderedDict()
-        toAppend["county"] = place[0]
-        toAppend["state"] = place[1]
-        for j in b[i]:
-            toAppend[dates[count]] = j
-            count += 1
+        toAppend["county"], toAppend["state"] = place[0], place[1]
+        for j in range(len(b[i])):
+            toAppend[dates[j]] = b[i][j]
         forwJson.append(toAppend)
     with open('BedsNeeded.json', mode='w+') as output:
         json.dump(forwJson,output)
-    
+
 if __name__ == '__main__':
 	main()
 
