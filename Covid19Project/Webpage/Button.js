0f688124a4cb85baf59e2ed82159cc531997b6b1
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-27 17:22:01 -0400

    Update format of webpage to fit on smaller screens

--- Covid19Project/Webpage/Button.js
+++ Covid19Project/Webpage/Button.js
@@ -1,6 +1,8 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/426DataTest/HospitalBedsMap?:display_count=y&publish=yes&:origin=viz_share_link";
+    url = "https://public.tableau.com/views/426DataTest/HospitalBedsMap?:retry=yes&:display_count=y&:origin=viz_share_link";
 
     var viz = new tableau.Viz(containerDiv, url);
 }   
+
+
