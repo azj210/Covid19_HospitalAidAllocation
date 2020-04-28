f72561f940cc4aa336af32913489c5530a750c87
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-28 14:25:56 -0400

    Add aggregate data section

--- Covid19Project/Webpage/Button.js
+++ Covid19Project/Webpage/Button.js
@@ -1,7 +1,18 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
     url = "https://public.tableau.com/views/426DataTest/HospitalBedsMap?:retry=yes&:display_count=y&:origin=viz_share_link";
+    //https://public.tableau.com/views/DeathRatesPopulationDemographics/DeathRatesCensus?:display_count=y&publish=yes&:origin=viz_share_link
+    //https://public.tableau.com/views/426DataTest/HospitalBedsMap?:retry=yes&:display_count=y&:origin=viz_share_link
+    var viz = new tableau.Viz(containerDiv, url);
+}   
 
+function initViz2() {
+    var containerDiv = document.getElementById("vizContainer2"),
+    url = "https://public.tableau.com/views/testing4_15880608226550/DeathRatesCensus?:display_count=y&publish=yes&:origin=viz_share_link";
+    //https://public.tableau.com/views/DeathRatesPopulationDemographics/DeathRatesCensus?:display_count=y&publish=yes&:origin=viz_share_link
+    //https://public.tableau.com/views/426DataTest/HospitalBedsMap?:retry=yes&:display_count=y&:origin=viz_share_link
+    //https://public.tableau.com/views/DeathRatesPopulationDemographics/DeathRatesCensus?:display_count=y&publish=yes&:origin=viz_share_link
+    //https://public.tableau.com/views/testing4_15880608226550/DeathRatesCensus?:display_count=y&publish=yes&:origin=viz_share_link
     var viz = new tableau.Viz(containerDiv, url);
 }   
 
