066331645240be2bc67b7620bb34615da7d302f2
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-28 17:13:40 -0400

    Working buttons

--- Covid19Project/Webpage/Button.js
+++ Covid19Project/Webpage/Button.js
@@ -16,4 +16,14 @@ function initViz2() {
     var viz = new tableau.Viz(containerDiv, url);
 }   
 
+function button1() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+function button2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function button3() {
+    document.getElementById("myDropdown3").classList.toggle("show2");
+}
