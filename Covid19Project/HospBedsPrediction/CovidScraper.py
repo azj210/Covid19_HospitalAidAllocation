a1719ff33acb69b80b83ef60e3bbec9b3cabadb6
azj210 <ajiang1098@gmail.com>
2020-06-15 03:30:10 -0400

    Changed consec days of decr cases calculation

--- Covid19Project/HospBedsPrediction/CovidScraper.py
+++ Covid19Project/HospBedsPrediction/CovidScraper.py
@@ -5,11 +5,13 @@ from datetime import date, timedelta
 import numpy as np
 import copy
 import json
+from statistics import mean
 
 def editDict(dictInput, link):
     #loop through the file and add the [confirmed,deaths] as a value to key county,state in dict
     r = request.urlopen(link).read().decode('utf8').split("\n")
     reader = csv.reader(r)
+    
     for line in reader:
         if len(line) > 2 and line[1] == "New York City":
             dictInput["new york" + ',' + line[2].lower()].append([line[7],line[8]])
@@ -17,20 +19,25 @@ def editDict(dictInput, link):
             #line[7] is confirmed and line[8] is deaths
             dictInput[line[1].lower() + ',' + line[2].lower()].append([line[7],line[8]])
 
+
 def predict(x, y, pred):
     #quadratic regression: predicting cases for pred days out
     coeffs = np.polyfit(x, y, 2)
     futureCases = []
+    
     for i in range(len(x), len(x)+pred):
         case = int(round((coeffs[0] * (i**2)) + (coeffs[1] * i) + coeffs[1]))
         if case <= 0:
             futureCases.append(0)
         else:
             futureCases.append(case)
+            
     return futureCases
+
 
 def generatePredict(tracker, condition, days):  
     res = defaultdict(int)
+    
     for i in tracker.keys():
         #error handling for if there is 2 days or less worth of data
         if len(tracker[i]) > 2:
@@ -43,22 +50,27 @@ def generatePredict(tracker, condition, days):
             else:
                 for j in range(1, len(data)):
                     cases.append(max(0,(int(data[j][1]) - int(data[j-1][1]))))  
+                    
             #calculate number of new cases or deaths "days" days out
             futureCases = predict(time, cases, days)
             cases += futureCases
             res[i] = cases
+            
     return res
+
 
 #c - time series of new cases from 3/22 to "days" days from the end date. d is corresponding new death time series
 def bedsNeeded(c, d, tracker, days):
     beds = defaultdict(list)
     dischargeAmt = {15:(1/4), 21:(1/3), 26:(1/2), 32:1}
+    
     #run simulation for each county
     for i in c.keys():
         #Of the initial reported numbers, around 75% will require hospital care
         beds[i].append(int(round(int(tracker[i][0][0]) * .75)))
         #0.3 of people need hospitalization and of those 0.3, it can be assumed that 75% will be discharged
         toDischarge = [round(z * 0.3 * 0.75) for z in copy.deepcopy(c[i])] 
+        
         #loop through all days from day 1 to today
         for j in range(len(c[i])):
             yestH, newHosp, deaths = beds[i][j], 0.3 * c[i][j], 0.8 * d[i][j] 
@@ -71,20 +83,25 @@ def bedsNeeded(c, d, tracker, days):
                     todayDischarge += discharge
                     toDischarge[j-k] -= discharge
             beds[i].append(max(0,(int(round(yestH + newHosp - deaths - todayDischarge)))))
+            
         if len(beds[i]) > days:
             beds[i].remove(beds[i][0])
         if len(beds[i]) < days:
             extra = [0] * (days - len(beds[i]))
             beds[i] = extra + beds[i]
+            
     return beds
 
+
 def allClear(cases, days, extra, mode):
-    isIncr, consecDays, consecTrack = defaultdict(bool), defaultdict(list), defaultdict(int)
+    isIncr, consecDays, consecTrack = defaultdict(bool), defaultdict(list), defaultdict(lambda: 0)
+    
     #init isIncr
     for i in cases.keys():
         if i.split(",")[1] not in isIncr:
             isIncr[i.split(",")[1]] = True
     toCheck, backCheck = (days - extra), 0
+    
     #check new cases by state
     while toCheck > 0:
         for i in cases.keys():
@@ -95,24 +112,30 @@ def allClear(cases, days, extra, mode):
                 consecDays[state][backCheck] += cases[i][backCheck]
         toCheck -= 1
         backCheck += 1
+    
+    if mode == 0:
+        return consecDays
+    
     #save consecutive days of decreasing new cases
     for i in consecDays.keys():
-        for j in range(len(consecDays[i])-1,-1,-1):
-            if j == len(consecDays[i]) - 1:
-                consecTrack[i] = 0
-            elif isIncr[i] and (consecDays[i][j] > consecDays[i][j+1]):
+        for j in range(len(consecDays[i])-1, 2, -1):
+            #if on a particular day the number of new cases is less than the avg of the number of new cases
+            #over the prior 3 days, then we add 1 to consecutive days of decreasing cases
+            if isIncr[i] and (consecDays[i][j] < mean(consecDays[i][j-3:j])):
                 consecTrack[i] += 1
             else:
                 isIncr[i] = False
-    if mode == 0:
-        return consecDays
+                break
+                
     return consecTrack
 
+
 def main():
-    start, end, delta = date(2020,3,22), date(2020,5,4), timedelta(days=1)
+    start, end, delta = date(2020,3,22), date(2020,6,14), timedelta(days=1)
     tracker = defaultdict(list)
     days, days2 = 3, 3
     dates = []
+    
 	#loop through all available covid files from 3/22/2020 to today
     while start <= end:
         thisDate = start.strftime('%m-%d-%Y')
@@ -126,6 +149,7 @@ def main():
         start += delta
     c, d = generatePredict(tracker, "cases", days2), generatePredict(tracker, "deaths", days2)
     b = bedsNeeded(c, d, tracker, days)
+    
     #converting bedsNeeded to json to read into spark dataframe
     forwJson = []
     for i in b.keys():
@@ -137,6 +161,7 @@ def main():
         forwJson.append(toAppend)
     with open('BedsNeeded.json', mode='w+') as output:
         json.dump(forwJson,output)
+        
     with open('NewCasesTrack.csv', mode='w+') as output:
         writer = csv.writer(output, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
         head = ["state"]
@@ -149,6 +174,7 @@ def main():
             a = info[i][len(info[i])-7:len(info[i])]
             a.reverse()
             writer.writerow([i] + a)
+            
     #converting consecutive days of decreasing cases to csv
     with open('DecrCases.csv', mode='w+') as output:
         clears = allClear(c, days, days2, 1)
@@ -157,9 +183,9 @@ def main():
         for i in clears.keys():
             writer.writerow([i, clears[i]])
             
-if __name__ == '__main__':
-	main()
 
+if __name__ == '__main__':
+    main()
 
 
 
